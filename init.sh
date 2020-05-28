772c3a905e25c3d734c0f3ddfa92f6a7c49a6801
BertiMario <mario.berti@studenti.unitn.it>
2020-05-28 14:10:18 +0200

    Add files via upload

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
-kind create cluster --name $USER
-export KUBECONFIG="$(kind get kubeconfig-path --name=$USER)"
+#kind create cluster --name $USER
+#export KUBECONFIG="$(kind get kubeconfig-path --name=$USER)"
 kubectl apply -f nginx_configMap.yaml
 kubectl apply -f alpine_ampache.yaml
 kubectl apply -f frontend.yaml
 kubectl apply -f hpa_web.yaml
+kubectl apply -f load_gen.yaml
